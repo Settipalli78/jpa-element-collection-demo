9edce59fb7505beceb3bc3941e37c2124186c2a7
Settipalli78 <settipalli.uk@gmail.com>
2017-12-16 19:09:45 +1400

    test

--- JavaProject/src/com/sample/java/SampleJava.java
+++ JavaProject/src/com/sample/java/SampleJava.java
@@ -3,26 +3,25 @@ package com.sample.java;
 import java.util.ArrayList;
 import java.util.List;
 
+import com.sample.java.bean.Student;
+
 public class SampleJava {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		
-		List<String> stringList=new ArrayList<String>();
-		
-		stringList.add("Prasad");
-		stringList.add("Settipalli");
-		stringList.add("GodhaSukeerthi");
-		stringList.add("Settipalli");
-		stringList.add("Geeth");
-		stringList.add("Geeth");
-		stringList.add("Geeth");
-		
-		for(String names: stringList) {
-			
+
+		List<Student> stdList = new ArrayList<Student>();
+
+		stdList.add(new Student(101, "Geeth", "Ukg"));
+		stdList.add(new Student(102, "Jigisha", "2nd"));
+		stdList.add(new Student(103, "Jessi", "1st"));
+		stdList.add(new Student(104, "Siri", "10Th"));
+		stdList.add(new Student(105, "Hani", "Lkg"));
+
+		for (Student names : stdList) {
+
 			System.out.println(names);
 		}
-		
 
 	}
 
